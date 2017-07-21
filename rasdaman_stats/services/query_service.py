990dd3aefc7023c33ffccf5cf429ff94ad1e91fd
Enrique Cornejo <enrique@cornejo.me>
2017-07-21 16:39:16 +0200

    WIP - Added stats endpoint

--- rasdaman_stats/services/query_service.py
+++ rasdaman_stats/services/query_service.py
@@ -5,8 +5,9 @@ from requests import Request, Session
 import logging
 from rasdaman_stats.errors import Error
 import tempfile
-
-from osgeo import gdal, ogr
+from rasterstats import zonal_stats
+from osgeo import gdal
+from gdalconst import *
 
 
 from CTRegisterMicroserviceFlask import request_to_microservice
@@ -19,18 +20,12 @@ def get_stats(config):
     # TODO: errors
     logging.info('[QueryService] Obtaining statistics')
 
-    request_url = CT_URL + '/' + API_VERSION + '/query/' + config.get('datasetId')
-    session = Session()
-
     logging.info('[QueryService] Getting mask from geostore')
     vector_mask = get_geostore(config)
-    logging.info(vector_mask)
+    logging.debug(vector_mask)
 
     bbox = vector_mask["data"]["attributes"]["bbox"]
-    logging.info("BBOX: " + ', '.join(str(coord) for coord in bbox))
-    
-    logging.info('[QueryService] Getting raster from rasdaman')
-    # Need to update the CT plugin to allow raw responses
+    logging.debug("BBOX: " + ', '.join(str(coord) for coord in bbox))
 
     fields = get_fields(config)
     coverage_name = fields["coverageId"]
@@ -40,10 +35,10 @@ def get_stats(config):
     for axis, datum in extra_axes_dct.items():
         extra_axes_str_arr.append( "".join([",", str(axis), "(\"", str(datum), "\")"]))
     extra_axes_str = "".join(extra_axes_str_arr)
-    
+
     # Only slicing - not subsetting
     logging.info(str(extra_axes_str))
-    
+
     query_array = [
         "for cov in (",
         coverage_name,
@@ -60,6 +55,12 @@ def get_stats(config):
     wcps_query = ''.join(query_array)
     logging.debug("WCPS: " + wcps_query)
 
+    logging.info('[QueryService] Getting raster from rasdaman')
+    # Need to update the CT plugin to allow raw responses
+
+    request_url = CT_URL + '/' + API_VERSION + '/query/' + config.get('datasetId')
+    session = Session()
+
     request = Request(
         method = "POST",
         url = request_url,
@@ -73,16 +74,24 @@ def get_stats(config):
     prepped = session.prepare_request(request)
     response = session.send(prepped)
 
-    with tempfile.NamedTemporaryFile(suffix='.tiff', delete=False) as f:
-        logging.info(f.name)
-        for chunk in response.iter_content(chunk_size=1024):
-            f.write(chunk)
-            
-        dataset = gdal.Open(f.name)
+
 
     
-
-    return dataset
+    with tempfile.NamedTemporaryFile(suffix='.tiff', delete=False) as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            f.write(chunk)
+        # dataset = gdal.Open(f.name)
+        with tempfile.NamedTemporaryFile(suffix='.geo.json', delete=False) as g:
+            logging.debug("GEOJSON")
+            encoded_data = json.dumps(vector_mask["data"]["attributes"]["geojson"])
+            logging.debug(encoded_data)
+            g.write(encoded_data.encode())
+            g.close()
+            logging.info("Files: " + f.name + " " + g.name)
+            stats = zonal_stats(g.name, f.name, nodata=2, all_touched=True)
+    logging.info("STATS")
+    logging.info(stats)
+    return stats
 
 def get_geostore(config):
     logging.info('[QueryService] Getting geostore')
@@ -91,7 +100,7 @@ def get_geostore(config):
             'uri': '/geostore/' + config.get('geostoreId'),
             'method': 'GET'
         }
-        response = request_to_microservice(request_options)        
+        response = request_to_microservice(request_options)
     except Exception as error:
         raise error
     return response
@@ -103,7 +112,7 @@ def get_fields(config):
             'uri': '/fields/' + config.get('datasetId'),
             'method': 'GET'
         }
-        response = request_to_microservice(request_options)        
+        response = request_to_microservice(request_options)
     except Exception as error:
         raise error
     return response
